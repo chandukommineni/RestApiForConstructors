2d4fc6af0ed1f872b359898bac0b88e8b3773526
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-03-15 20:58:19 +0530

    rest api with springboot finished

--- src/main/java/api/RestAPI/controllers/ConstructorController.java
+++ src/main/java/api/RestAPI/controllers/ConstructorController.java
@@ -3,7 +3,9 @@ package api.RestAPI.controllers;
 import api.RestAPI.model.Constructor;
 import api.RestAPI.services.ConstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.constructor.Construct;
 
 import java.util.List;
 
@@ -12,32 +14,41 @@ import java.util.List;
 public class ConstructorController {
     @Autowired
     ConstructorService service;
+    @GetMapping("/getData")
+    public ResponseEntity<List<Constructor>> getAllData(){
+        return service.getAllDetails();
+    }
+    @GetMapping("getData/name/{name}")
+    public ResponseEntity<List<Constructor>>getDataByName(@PathVariable String name){
+        return service.getDetailsByName(name);
+    }
+    @GetMapping("/getDatabyRating")
+    public ResponseEntity<List<Constructor>> getDatabyRating(){
+        return service.getAllDetailsByRank();
+    }
+    @GetMapping("/getData/id/{id}")
+    public ResponseEntity<Constructor> getDatabyId(@PathVariable("id") int id){
 
+        return service.getDetailsbyId(id);
+    }
     @PostMapping("/setData")
-    public Constructor setData(@RequestBody Constructor obj){
+    public ResponseEntity<Constructor> setData(@RequestBody Constructor obj){
 
-        Constructor constructor = service.setDetails(obj);
-        return constructor;
+         return service.setDetails(obj);
 
     }
 
     @PutMapping("/update/{id}")
-    public Constructor updateData(@RequestBody Constructor obj,@PathVariable("id") Integer id){
-        return service.updateDetails(obj,id);
+    public ResponseEntity<Constructor>  updateData(@PathVariable("id") int id,@RequestBody Constructor obj){
+        return service.updateDetails(id,obj);
     }
 
-    @GetMapping("/getData")
-    public List<Constructor> getAllData(){
-        List<Constructor> allDetails = service.getAllDetails();
-        return allDetails;
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Constructor> deleteDate(@PathVariable("id") int id){
+
+        return service.deleteDetails(id);
     }
-    @GetMapping("/getData/{name}")
-    public List<Constructor> getDataByName(@PathVariable String name){
-        return service.getDetailsByName(name);
-    }
-    @GetMapping("/getDatabyRating")
-    public List<Constructor> getDatabyRating(){
-        return service.getAllDetailsByRank();
-    }
+
+
 
 }
--- src/main/java/api/RestAPI/services/ConstructorService.java
+++ src/main/java/api/RestAPI/services/ConstructorService.java
@@ -3,6 +3,8 @@ package api.RestAPI.services;
 import api.RestAPI.model.Constructor;
 import api.RestAPI.repositories.constructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,23 +15,58 @@ public class ConstructorService {
     @Autowired
     constructorRepository repo;
 
-    public Constructor setDetails(Constructor obj)
+    public ResponseEntity<List<Constructor>> getAllDetails(){
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<Constructor>> getAllDetailsByRank(){
+        return new ResponseEntity<>(repo.findTopByratingDesc(),HttpStatus.OK);
+
+    }
+    public ResponseEntity<Constructor> getDetailsbyId(int id){
+        Constructor con= repo.findById(id).orElse(null);
+        if (con!=null){
+            return new ResponseEntity<>(con,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    public ResponseEntity<List<Constructor>> getDetailsByName(String name){
+        return new ResponseEntity<>(repo.findByname(name),HttpStatus.OK);
+    }
+
+
+    public ResponseEntity<Constructor> setDetails(Constructor obj)
     {
-        return repo.save(obj);
+        return new ResponseEntity<>(repo.save(obj),HttpStatus.CREATED);
     }
 
-    public List<Constructor> getAllDetails(){
-        return repo.findAll();
+    public  ResponseEntity<Constructor> updateDetails(int id,Constructor obj){
+        Constructor con=repo.findById(id).orElse((null));
+        if (con!=null){
+            obj.setId(id);
+            return new ResponseEntity<>(repo.save(obj),HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    public Optional<Constructor> getDetailsbyId(int id){
-        return repo.findById(id);
+    public ResponseEntity<Constructor> deleteDetails(int id){
+        Constructor con=repo.findById(id).orElse((null));
+        if (con!=null){
+            repo.delete(con);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
-    public List<Constructor> getDetailsByName(String Name){
-        return repo.findByname(Name);
-    }
-    
 
 
 
